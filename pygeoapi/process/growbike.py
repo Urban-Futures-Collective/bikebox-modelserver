3ca7ec1d19443e3618cbb4ecbad4e7eb31ba0ec7
Manuel-Knepper <manuel.knepper@gmx.net>
2026-06-22 20:45:39 +0200

    changed parameter default values and provided more in depth descriptions.

--- pygeoapi/process/growbike.py
+++ pygeoapi/process/growbike.py
@@ -13,14 +13,14 @@ PROCESS_METADATA = {
     'inputs': {
         'cityname': {
             'title': 'City Name',
-            'description': 'Name of city to run analysis on',
+            'description': 'Name of the city that the analysis should be performed on. This is the query string used to fetch the data from nominatim.',
             'schema': {
                 'type': 'string'
             },
         },
-        'proj_crs': {
-            'title': 'Proj crs',
-            'description': 'CRS to be used for projection',
+        'crs_projected': {
+            'title': 'CRS projected',
+            'description': 'EPSG code of the coordinate reference system that is used to project osm data. Default is 3857 (WGS 84 / Pseudo-Mercator). If this web mercator projection is not needed, then for Europe 3035 (LAEA) and globally 54035 (Equal Earth) is better.',
             'schema': {
                 'type': 'string',
                 'default': '3857'
@@ -28,7 +28,7 @@ PROCESS_METADATA = {
         },
         'ranking': {
             'title': 'Ranking',
-            'description': 'measure to be used for ranking edges',
+            'description': 'Method used to rank edges. Must be betweenness_centrality (default), closeness_centrality, or random.',
             'schema': {
                 'type': 'string',
                 'default': 'betweenness_centrality'
@@ -36,31 +36,31 @@ PROCESS_METADATA = {
         },
         'seed_point_type': {
             'title': 'Seed point type',
-            'description': 'what seed points to use for generation of network',
+            'description': 'If set to auto, selects grid_square or grid_triangle automatically depending on the street networks orientation entropy. If set to grid_square, creates a square grid. If set to rail, uses railway stations and halts. If set to school, uses kindergartens, schools, colleges, and universities. If set to park, uses parks, gardens, nature reserves, and public bathing places.',
             'schema': {
                 'type': 'string',
-                'default': 'grid'
+                'default': 'auto'
             },
         },
         'seed_point_grid_spacing': {
             'title': 'Seed point grid spacing',
-            'description': 'if grid is used this is the spacing between seed points, in meters',
+            'description': 'If seed_point_type is set to grid_square or grid_triangle, this is the spacing between seed points, in meters. Auto-value for seed_point_type grid_square with seed_point_linking triangulate_delaunay is 1707. Auto-value for seed_point_type grid_square with seed_point_linking quadrangulate is 1000. Auto-value for seed_point_type grid_triangle is 1154',
             'schema': {
                 'type': 'integer',
-                'default': 1707
+                'default': 'auto'
             },
         },
         'seed_point_delta': {
             'title': 'Seed point delta',
-            'description': 'Maximum distance between seed points and osm nodes',
+            'description': 'Maximum distance between raw seed points and osm nodes for snapping, in meters. Auto-value is round(seed_point_grid_spacing/4).',
             'schema': {
                 'type': 'integer',
-                'default': 500
+                'default': 'auto'
             },
         },
         'existing_network_spacing': {
             'title': 'Existing network spacing',
-            'description': 'spacing between seed points with existing network. If not set to positive integer existing network is ignored',
+            'description': 'Spacing between seed points, in meters, only on the existing bicycle network. If not set to a positive integer, the existing network is ignored. existing_network_spacing is recommended to be smaller than seed_point_grid_spacing, ideally around 25%, to ensure that the existing bicycle network is built first.',
             'schema': {
                 'type': 'integer',
                 'default': None
@@ -88,7 +88,9 @@ PROCESS_METADATA = {
 
 
 class GrowBikeNet(BaseProcessor):
-    """My custom pygeoapi process"""
+    """
+    Process for execution of GrowBikeNet algorithm from growbikenet package. The algorithm builds a bicycle network within a given city either from scratch or while considering the already existing infrastructure.
+    """
 
     def __init__(self, processor_def):
         super().__init__(processor_def, PROCESS_METADATA)
@@ -97,20 +99,21 @@ class GrowBikeNet(BaseProcessor):
 
     def execute(self, data, outputs=None):
         """
-        Execute process
+        Execute process. Reads parameters given by user and uses default values if no parameters are provided.
         """
         city_name = data.get('cityname')
-        proj_crs = data.get('proj_crs', '3857')
+        crs_projected = data.get('crs_projected', '3857')
         ranking = data.get('ranking', 'betweenness_centrality')
-        seed_point_type = data.get('seed_point_type', 'grid')
-        seed_point_grid_spacing = data.get('seed_point_grid_spacing', 1707)
-        seed_point_delta = data.get('seed_point_delta', 500)
+        seed_point_type = data.get('seed_point_type', 'auto')
+        seed_point_grid_spacing = data.get('seed_point_grid_spacing', 'auto')
+        seed_point_delta = data.get('seed_point_delta', 'auto')
         existing_network_spacing = data.get('existing_network_spacing', None)
         export_data = data.get('export_data', False)
 
         try:
-            gdf = gbn.growbikenet(city_name=city_name, proj_crs=proj_crs, ranking=ranking, seed_point_type=seed_point_type, seed_point_grid_spacing=seed_point_grid_spacing,seed_point_delta=seed_point_delta, existing_network_spacing=existing_network_spacing, export_data=export_data)
+            gdf = gbn.growbikenet(city_name=city_name, crs_projected=crs_projected, ranking=ranking, seed_point_type=seed_point_type, seed_point_grid_spacing=seed_point_grid_spacing,seed_point_delta=seed_point_delta, existing_network_spacing=existing_network_spacing, export_data=export_data)
 
+            # geodataframe with results gets converted to CRS 'epsg:4326' since this is the standard for GeoJson: https://docs.ropensci.org/geojsonio/articles/geojson_spec.html
             gdf = gdf.to_crs('epsg:4326')
 
             result = gdf.__geo_interface__
